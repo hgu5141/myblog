f888d0b4316aeffb89e4effd7490b9a1b843dd33
hgu5141 <hgu5141@naver.com>
2022-01-21 20:18:19 +0900

    index.html 테이블 추가

--- src/main/java/com/hanghae/myblog/controller/BlogController.java
+++ src/main/java/com/hanghae/myblog/controller/BlogController.java
@@ -1,4 +1,5 @@
 package com.hanghae.myblog.controller;
 
 public class BlogController {
+
 }
--- src/main/java/com/hanghae/myblog/domain/Blog.java
+++ src/main/java/com/hanghae/myblog/domain/Blog.java
@@ -1,4 +1,29 @@
 package com.hanghae.myblog.domain;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class Blog {
+    @GeneratedValue(strategy = GenerationType.AUTO) // 자동 증가 명령어
+    @Id // id값 기본키로 사용
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    public Blog(String username, String title, String content){
+        this.username = username;
+        this.title = title;
+        this.content = content;
+    }
+
+
 }
